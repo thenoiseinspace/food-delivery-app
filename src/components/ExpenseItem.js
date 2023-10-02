7022726a648abfd41cad0e3834817e8dded7b802
Sarah Gilbreath <thenoiseinspace@gmail.com>
2023-10-02 17:00:40 -0400

    adding initial styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,13 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
   return (
-    <div>
-      <div>Date</div>
-      <div>Title</div>
-      <div>Amount</div>
+    <div className="expense-item">
+      <div>March 28, 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
+      </div>
     </div>
   );
 }
